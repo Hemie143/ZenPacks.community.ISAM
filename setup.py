9d06e9a86c02368b49e2f5a3ee7bf1abc6d44ffa
Laurent Hemeryck <hemie143@gmail.com>
2018-05-30 14:05:50 +0200

    Changed datapoints for status to LAST

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.ISAM"
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 AUTHOR = "Laurent Hemeryck"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
